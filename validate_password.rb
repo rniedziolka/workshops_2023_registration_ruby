b6a87d4cbcfa0acbc6efd106639cbec6a1a1e5d4
rniedziolka <r.m.niedziolka@gmail.com>
2023-05-01 13:29:25 +0200

    solution-of-the-second-task

--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,9 @@
 class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
 
+  def perform
+    @password =~ /^(?=.{6,24}$)(?=.*[a-z])(?=.*[A-Z])(?=[^0-9]*[0-9])(?=.*[!@#$%&*+=:;?<>])(?:([\w\d*?!:;])\1?(?!\1))+$/ ? true : false
+  end
 end
