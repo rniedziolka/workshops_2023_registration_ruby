cfb61cc7fa98fbc3f6e676dc438263d18fdec37f
rniedziolka <r.m.niedziolka@gmail.com>
2023-05-01 13:28:33 +0200

    solution-to-the-first-task

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,11 @@
 class CountElements
+  def initialize(array)
+    @array = array
+  end
 
+  def perform
+    @array.each_with_object(Hash.new(0)) do |element, new_hash|
+      new_hash[element] += 1
+    end
+  end
 end
